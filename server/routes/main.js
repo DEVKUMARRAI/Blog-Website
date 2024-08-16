a37c1d2a3ed204933f3287d05dff55e1322e5bda
DEV RAI <devrai197551@gmail.com>
2024-08-16 22:51:13 +0530

    View Engine and Static Files Setup Done

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,8 @@ import express from 'express'
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.send('Hey There Dev');
+    const data = 'Dev'
+    res.render('index',{data});
 })
 
 
